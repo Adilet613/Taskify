56660fb73d0a0ceba1d1cb2178ddd1c756c88ecf
Adilet613 <sungatulyadilet@gmail.com>
2025-03-27 03:40:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-function selectRole(role) {
-    document.getElementById("roleSelection").style.display = "none";
-
-    if (role === "customer") {
-        document.getElementById("workSelection").style.display = "block";
-    } else {
-        alert("Функция для фрилансеров пока не добавлена.");
-    }
+function showCustomerMenu() {
+    document.querySelector('.role-selection').style.display = 'none';
+    document.getElementById('customer-menu').style.display = 'block';
 }
 
-function selectWork(work) {
-    document.getElementById("workSelection").style.display = "none";
-    document.getElementById("chatSection").style.display = "block";
+function goBack() {
+    document.getElementById('customer-menu').style.display = 'none';
+    document.querySelector('.role-selection').style.display = 'block';
 }
