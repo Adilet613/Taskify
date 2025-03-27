f6b694a1122c5f1e962224fc88aa368a29b1c092
Adilet613 <sungatulyadilet@gmail.com>
2025-03-27 15:57:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function showCustomerMenu() {
     document.getElementById("customer-menu").classList.remove("hidden");
+    document.getElementById("work-description").classList.add("hidden");
     document.getElementById("freelancer-test").classList.add("hidden");
 }
 
 function showFreelancerTest() {
     document.getElementById("customer-menu").classList.add("hidden");
+    document.getElementById("work-description").classList.add("hidden");
     document.getElementById("freelancer-test").classList.remove("hidden");
+}
+
+function selectWork(work) {
+    document.getElementById("customer-menu").classList.add("hidden");
+    document.getElementById("work-title").textContent = work;
+    document.getElementById("work-description").classList.remove("hidden");
 }
