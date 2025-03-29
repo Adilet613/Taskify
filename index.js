5b12da50c71a6a40a94a2b17f3f35c274dd3afac
Adilet613 <sungatulyadilet@gmail.com>
2025-03-29 21:48:52 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("customer-btn").addEventListener("click", function() {
+    window.location.href = "choose-job.html";
+});
+
+document.getElementById("worker-btn").addEventListener("click", function() {
+    window.location.href = "test.html";
+});
+
+document.getElementById("login-btn").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+
+document.getElementById("register-btn").addEventListener("click", function() {
+    window.location.href = "register.html";
+});
