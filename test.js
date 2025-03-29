dca9b794cbbd0afd8aa8b3a4d98350f4d657fa9d
Adilet613 <sungatulyadilet@gmail.com>
2025-03-29 17:58:50 +0500

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ document.getElementById("submit-test").addEventListener("click", function () {
     let answer2 = document.getElementById("answer2").value.trim().toLowerCase();
 
     if (answer1 === "правильный ответ" && answer2 === "правильный ответ") {
-        localStorage.setItem("isFreelancer", "true");
+        localStorage.setItem("isFreelancer", "true"); // Сохраняем в localStorage
         alert("Поздравляем! Вы прошли тест.");
         window.location.href = "orders.html"; // Отправляем на страницу с заказами
     } else {
